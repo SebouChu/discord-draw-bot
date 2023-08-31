653b0c4edf72c8b216caeeb326d04ecd209f7ce0
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-08-31 16:03:47 +0200

    replaced with_reaction with manual stupid and ugly loop till the lib is fixed

--- script.rb
+++ script.rb
@@ -6,6 +6,8 @@ bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_BOT_TOKEN'], prefi
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
+byebug
+
 bot.command(:draw, min_args: 2, max_args: 2, usage: 'draw <message_url> <emoji reaction>') do |event, messageUrl, reaction|
   event.respond "Exporting users who reacted #{reaction} to #{messageUrl} ..."
 
@@ -25,13 +27,21 @@ bot.command(:draw, min_args: 2, max_args: 2, usage: 'draw <message_url> <emoji r
     return
   end
 
-  users = message.reacted_with(reaction, limit: nil)
+  users = []
+  resp = Discordrb::API::Channel.get_reactions(bot.token, channelId.to_i, messageId.to_i, reaction, nil, after_id, 100)
+  new_users = JSON.parse(resp)
+  users.concat new_users
+  while new_users.size > 0
+    resp = Discordrb::API::Channel.get_reactions(bot.token, channelId.to_i, messageId.to_i, reaction, nil, users.last["id"], 100)
+    new_users = JSON.parse(resp)
+    users.concat new_users
+  end
 
   Tempfile.new(['users', '.csv']).tap do |file|
     CSV.open(file, 'wb') do |csv|
       csv << ["id", "username"]
       users.each do |user|
-        csv << [user.id, user.username]
+        csv << [user["id"], user["username"]]
       end
     end
     event.channel.send_file(file)
